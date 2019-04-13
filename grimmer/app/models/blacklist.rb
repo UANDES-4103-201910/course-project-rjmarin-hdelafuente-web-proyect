c8fe9bef8794aff73e1863bd0c0849391ff0f481
hdelafuente <hadelafuente@miuandes.c>
2019-04-12 20:32:27 -0400

    master: * Email callback for users

--- grimmer/app/models/blacklist.rb
+++ grimmer/app/models/blacklist.rb
@@ -1,3 +1,14 @@
 class Blacklist < ApplicationRecord
   belongs_to :user
+
+  before_create :check_email
+  private
+  def check_email
+    if email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      return true
+    else
+      errors.add(:email, "not a valid email")
+      return false
+    end
+  end
 end
