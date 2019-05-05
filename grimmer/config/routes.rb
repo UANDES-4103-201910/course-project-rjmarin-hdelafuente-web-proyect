d21b585b35fdd0eea65f0f9fc08f24acb848ddfe
hdelafuente <hadelafuente@miuandes.c>
2019-05-05 11:46:53 -0400

    master: * Added route to index, now it is posts index * Added Bootstrap 4

--- grimmer/config/routes.rb
+++ grimmer/config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :user_profiles
   resources :users
+
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
