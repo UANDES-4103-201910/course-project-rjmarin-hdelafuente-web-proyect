44677047fdeb31860ed5f10bab6dfd170db8e40f
Hugo De la Fuente Decebal-Cuza <hugodelafuentedecebal-cuza@Hugos-MacBook-Pro.local>
2019-06-09 13:35:26 -0400

    master: * Mock up for the search view * Can search matches in users name, post titles and descriptions

--- grimmer/app/controllers/search_controller.rb
+++ grimmer/app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
     if params[:search].blank?
       redirect_to(root_path, alert: "Search field empty!")
     else
-      @users = User.where(["name or city LIKE ?", "%#{params[:search]}%"])
+      @users = User.where(["name LIKE ?", "%#{params[:search]}%"])
       @posts = Post.where(["title or description LIKE ?", "%#{params[:search]}%"] )
     end
   end
