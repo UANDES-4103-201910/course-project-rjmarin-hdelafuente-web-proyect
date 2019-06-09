cea70067c880fa2400b031525178796c1e86a168
Hugo De la Fuente Decebal-Cuza <hugodelafuentedecebal-cuza@Hugos-MacBook-Pro.local>
2019-06-09 02:10:37 -0400

    geofence migrate

--- grimmer/db/schema.rb
+++ grimmer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_035908) do
+ActiveRecord::Schema.define(version: 2019_06_09_051935) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2019_06_08_035908) do
     t.string "region"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "latitude"
+    t.decimal "longitude"
     t.index ["user_id"], name: "index_geofences_on_user_id"
   end
 
