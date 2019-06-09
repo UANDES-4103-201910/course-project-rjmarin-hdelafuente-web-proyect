22870740d28523b845a7b76292c67faf4e7e29e9
Hugo De la Fuente Decebal-Cuza <hugodelafuentedecebal-cuza@Hugos-MacBook-Pro.local>
2019-06-09 00:59:14 -0400

    master: * Create report checks how many times the post has been reported * Can take out a post from the dumpster * Missing a view to show the post from the dumpster administration

--- grimmer/app/controllers/dumpsters_controller.rb
+++ grimmer/app/controllers/dumpsters_controller.rb
@@ -5,70 +5,50 @@ class DumpstersController < ApplicationController
   # GET /dumpsters.json
   def index
     @dumpsters = Dumpster.all
+
   end
 
-  # GET /dumpsters/1
-  # GET /dumpsters/1.json
   def show
-  end
-
-  # GET /dumpsters/new
-  def new
-    @dumpster = Dumpster.new
-  end
-
-  # GET /dumpsters/1/edit
-  def edit
   end
 
   # POST /dumpsters
   # POST /dumpsters.json
   def create
-    @dumpster = Dumpster.new(dumpster_params)
-
-    respond_to do |format|
-      if @dumpster.save
-        format.html { redirect_to @dumpster, notice: 'Dumpster was successfully created.' }
-        format.json { render :show, status: :created, location: @dumpster }
-      else
-        format.html { render :new }
-        format.json { render json: @dumpster.errors, status: :unprocessable_entity }
-      end
+    Post.update(params[:post_id], visibility: false)
+    @dumpster = Dumpster.create(post_id: params[:post_id])
+    if @dumpster.save
+      flash[:success] = "The post is now in the dumpster."
+      redirect_to admin_path
+    else
+      flash[:error] = "Couldn't send the post to the dumpster"
+      redirect_to admin_path
     end
-  end
 
-  # PATCH/PUT /dumpsters/1
-  # PATCH/PUT /dumpsters/1.json
-  def update
-    respond_to do |format|
-      if @dumpster.update(dumpster_params)
-        format.html { redirect_to @dumpster, notice: 'Dumpster was successfully updated.' }
-        format.json { render :show, status: :ok, location: @dumpster }
-      else
-        format.html { render :edit }
-        format.json { render json: @dumpster.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /dumpsters/1
   # DELETE /dumpsters/1.json
   def destroy
+    Post.update(params[:post_id], visibility: true )
+    @dumpster = Dumpster.find_by(params[:post_id])
     @dumpster.destroy
-    respond_to do |format|
-      format.html { redirect_to dumpsters_url, notice: 'Dumpster was successfully destroyed.' }
-      format.json { head :no_content }
+    if @dumpster.destroy
+      flash[:success] = "The post is visible again"
+      redirect_to admin_path
+    else
+      flash[:error] = "Couldn't make post visible"
+      redirect_to admin_path
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_dumpster
-      @dumpster = Dumpster.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_dumpster
+    @dumpster = Dumpster.find_by(post_id: params[:post_id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def dumpster_params
-      params.require(:dumpster).permit(:post_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def dumpster_params
+    params.require(:dumpster).permit(:post_id)
+  end
 end
